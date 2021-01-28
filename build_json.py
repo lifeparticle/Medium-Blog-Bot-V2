f82cf92dbfeba3354ead46947a4453a9b40afc36
lifeparticle <mahbubzaman17@gmail.com>
2021-01-28 20:08:48 +1100

    rm url to link

--- build_json.py
+++ build_json.py
@@ -10,7 +10,7 @@ def compare_data(oldData, newData):
 	last_pub_date = oldData[-1]['pubDate']
 	for nD in newData:
 		if nD['pubDate'] > last_pub_date:
-			data.insert(0, {"title": nD['title'], "url": nD['link'], "pubDate": nD['pubDate']})
+			data.insert(0, {"title": nD['title'], "link": nD['link'], "pubDate": nD['pubDate']})
 		else:
 			break
 	return data
